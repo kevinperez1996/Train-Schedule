89083b08f299c12fc8380959ea88cfaf665dc3e8
Kevin Perez <kevinperez996@outlook.com>
2019-09-24 17:28:58 -0400

    Discovered a bug where next train time will not update if the current time if after the next train time

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,6 +1,6 @@
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyBapUEDgkEs7Y0_fEW6aDS5fVBdYUVRAgc",
     authDomain: "train-schedule-8e0b9.firebaseapp.com",
     databaseURL: "https://train-schedule-8e0b9.firebaseio.com",
@@ -8,14 +8,14 @@
     storageBucket: "",
     messagingSenderId: "740256144304",
     appId: "1:740256144304:web:770b4144d9d4416e38d475"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  //When Sumbit is clicked
-  $("#add-train-btn").on("click", function (event){
+//When Sumbit is clicked
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
     var trainName = $("#train-name-input").val().trim();
@@ -23,32 +23,32 @@
     var trainTime = $("#first-train-input").val().trim();
     var trainFreq = $("#frequency-input").val().trim();
 
-    
-    
+
+
     //Object to hold all values --- will be used to push and store data in database
     var addedTrain = {
         train: trainName,
-        destination : trainDest, 
+        destination: trainDest,
         time: trainTime,
         frequency: trainFreq
     };
-    
-    database.ref().push(addedTrain); 
-    
-   // console.log(addedTrain.train);
+
+    database.ref().push(addedTrain);
+
+    // console.log(addedTrain.train);
     //console.log(addedTrain.destination);
     //console.log(addedTrain.time);
-   // console.log(addedTrain.frequency);
-    
-    
+    // console.log(addedTrain.frequency);
+
+
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
 
-  });
+});
 
-  database.ref().on("child_added", function (snapShot){
+database.ref().on("child_added", function (snapShot) {
     //console.log(snapShot.val());
 
     var trainName = snapShot.val().train;
@@ -61,28 +61,34 @@
     console.log("First Train Time: " + trainTime);
     console.log("Train Frequency: " + trainFreq);
 
-    var tableRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDest),
-        $("<td>").text(trainFreq),
-    );
 
-    $("#train-table").append(tableRow);
-    
+
+
     //get the current time
     var currentTime = moment().format("HH:mm");
     console.log("Current Time: " + currentTime);
 
     //calculate the next arrival using the first train time and train frequency
-    var NextArrival = moment(currentTime, "HH:mm").add(trainFreq, "minutes").format("HH:mm");
-    console.log( "Next Train: " + NextArrival);
 
-   // var difference = moment().diff(moment(trainTime, "HH:mm"), "minutes");
-   
+    var nextArrival = moment(trainTime, "HH:mm").add(trainFreq, "minutes").format("HH:mm");
+    console.log("Next Train: " + nextArrival);
     
-   //calculate dif between first train and now
-    var minutesAway = moment().diff(moment(NextArrival, "HH:mm"), "minutes");
+    var firsttime = moment(trainTime, "HH:mm").format("HH:mm");
+
+    
+    
+    //calculate dif between first train and now
+    var minutesAway = moment().diff(moment(nextArrival, "HH:mm"), "minutes");
     console.log(minutesAway);
-    
+
+    var tableRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway)
+    );
+
+    $("#train-table").append(tableRow);
 });
 
